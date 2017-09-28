6b66856ddbfd1356561d6e25668cf13ba3519030
ASO1601031 <32320658+ASO1601031@users.noreply.github.com>
2017-09-28 10:37:30 +0900

    初めてのコミット

---
+++ app/src/main/java/com/example/fukuharahiroshi2/mynamecard/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.fukuharahiroshi2.mynamecard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickButton(View view){
+        TextView textView = (TextView)findViewById(R.id.name);
+        textView.setText("一気飲み");
+    }
+}
